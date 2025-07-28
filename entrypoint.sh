85cba8983bbbad43c5f379520c576432272afb3a
HimanshuSolo2005 <himanshu.pv.verma@gmail.com>
2025-07-28 11:46:33 +0530

    implicitly added render file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,12 @@
+#!/bin/bash
 
 ollama serve &
 
+echo "Waiting for Ollama server to start..."
 sleep 5
 
+echo "Pulling LLaMA3 model..."
 ollama pull llama3
 
+echo "Starting app..."
 python main.py
